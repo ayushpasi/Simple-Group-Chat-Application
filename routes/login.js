ed6750b0b32df5c8f0abbd6492aad05aa23bb18b
ayushpasi <pasiayush999@gmail.com>
2023-10-17 22:34:38 +0530

    simple chat app completed

--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,42 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
   res.send(`
-    <form action='/' method='POST'>
-      <input type='text' name='name' id='name' placeholder='name'>
+    <form action='/' method='GET' onsubmit="localStorage.setItem('username', document.getElementById('username').value)">
+      <input type='text' name='username' id='username' placeholder='name'>
       <button type='submit'>Login</button>
-    </form>
-    <script>
-      // Store the username in localStorage when the form is submitted
-      document.querySelector('form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        const username = document.getElementById('name').value;
-        localStorage.setItem("username", username);
-       
-      });
-    </script>
-  `);
+    </form>`);
 });
 
-router.post("/", (req, res) => {
-  res.send(`<form action='/' method='POST'>
-  <input type='text' name='message' id='message' placeholder='message'>
-  <button type='submit'>Login</button>
-</form>`);
-  
-       const obj = req.body;
-  
-  res.send("Data has been received and processed on the server");
+router.get("/", (req, res) => {
+  fs.readFile("message.txt", 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      data = "no chat found";
+    }
+
+    res.send(`</br>${data}</br><form action='/' method='POST' onsubmit="document.getElementById('username').value=localStorage.getItem('username')">
+      <input type='text' name='message' id='message' placeholder='message'>
+      <input hidden type='text' name='username' id='username' placeholder='message'>
+      <button type='submit'>send</button>
+    </form>`);
+  });
+  });
+
+router.post("/",(req,res,next)=>{
+  let username=req.body.username;
+let message=req.body.message;
+let data= username+": "+message;
+fs.writeFile("message.txt", data, { flag: 'a' }, (err) => {
+  if (err) {
+    console.error("Error writing to file: " + err);
+  } else {
+    console.log("Data appended to file successfully.");
+  }
+});
+res.redirect('/');
 });
 
 module.exports = router;
